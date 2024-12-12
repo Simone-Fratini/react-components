24cd9042b3dfd0b4f6b182ba6fd9ecda57020a7d
Simone-Fratini <simone@fratini.com>
2024-12-12 18:29:47 +0100

    Add pasta data module with recipes and details

---
+++ src/data/food.jsx
@@ -0,0 +1,47 @@
+
+const pasta = [
+  {
+    id: 1,
+    title: "Carbonara",
+    ingredients: ["Pasta", "Guanciale", "Uova", "Pecorino", "Pepe nero"],
+    cookingTime: "20 min",
+    img: "./public/pasta/pastaCarbonara.jpg"
+  },
+  {
+    id: 2,
+    title: "Pasta e Fagioli",
+    ingredients: ["Pasta", "Fagioli", "Aglio", "Rosmarino", "Olio d'oliva", "Pomodoro"],
+    cookingTime: "40 min",
+    img: "./public/pasta/pastaFagioli.jpg"
+  },
+  {
+    id: 3,
+    title: "Pasta al Nero di Seppia",
+    ingredients: ["Pasta", "Nero di seppia", "Aglio", "Olio d'oliva", "Peperoncino"],
+    cookingTime: "30 min",
+    img: "./public/pasta/pastaNerodiSeppia.jpg"
+  },
+  {
+    id: 4,
+    title: "Pasta al Pomodoro",
+    ingredients: ["Pasta", "Pomodori", "Aglio", "Basilico", "Olio d'oliva"],
+    cookingTime: "25 min",
+    img: "./public/pasta/pastaPomodoro.jpg"
+  },
+  {
+    id: 5,
+    title: "Pasta al Tartufo",
+    ingredients: ["Pasta", "Tartufo", "Burro", "Pecorino", "Sale"],
+    cookingTime: "20 min",
+    img: "./public/pasta/pastaTartufo.jpg"
+  },
+  {
+    id: 6,
+    title: "Pasta alla Norma",
+    ingredients: ["Pasta", "Melanzane", "Pomodori", "Ricotta salata", "Basilico", "Olio d'oliva"],
+    cookingTime: "35 min",
+    img: "./public/pasta/pastaNorma.jpg"
+  }
+];
+
+export default pasta;
